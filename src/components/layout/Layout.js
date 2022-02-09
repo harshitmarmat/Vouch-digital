d88ff4513328f3a2368b687a0e19a390409c5d81
harshitmarmat <you@example.com>
2022-02-10 00:45:02 +0530

    Project Setup

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import {Layout} from 'antd'
+
+const { Header , Content , Footer} = Layout;
+
+const Structure = () =>{
+    return(
+        <Layout>
+            <Header >Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    )
+}
+
+export default Structure;
